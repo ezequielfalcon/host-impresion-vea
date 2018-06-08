89a00971ba86c2d696fc2f27a43e5227e143faa5
Ezequiel Falcón <falconezequiel@gmail.com>
2018-06-08 19:21:43 -0300

    impresión síncrona piola

---
+++ host.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const escpos = require('escpos');
+const device  = new escpos.Network('172.16.1.10');
+const options = { encoding: "windows1252" /* default */ };
+const printer = new escpos.Printer(device, options);
+
+
+function imprimir(nPedido, items) {
+  device.open(() => {
+    printer
+      .font('a')
+      .align('ct')
+      .style('bu')
+      .size(2, 2)
+      .text('Pedido #' + nPedido)
+      .control('LF')
+      .text('LAESO CHASCOMUS')
+      .control('LF')
+      .control('LF')
+      .font('a')
+      .align('lt')
+      .style('bu')
+      .size(1, 1);
+    for (const item of items) {
+      printer
+        .text(item);
+    }
+    printer
+      .font('a')
+      .align('ct')
+      .style('bu')
+      .size(2, 2)
+      .control('LF')
+      .control('LF')
+      .text('-----------')
+      .control('LF')
+      .control('LF')
+      .cut('full')
+      .close();
+  });
+}
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.header("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET, OPTIONS");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set('port', process.env.PORT || 5050);
+
+app.get('/', (req, res) => {
+  res.json({
+    mensaje: "Host de Impresión VEA"
+  })
+});
+
+app.post('/imprimir', (req, res) => {
+  if (req.body.npedido && req.body.items) {
+    const itemsPedido = JSON.parse(req.body.items);
+    imprimir(itemsPedido);
+    res.json({mensaje: "Ticket impreso!!"});
+  } else {
+    res.status(400).end();
+  }
+})
+
+app.listen(app.get('port'), () => {
+  console.log('Host de impresión escuchando en puerto ', app.get('port'));
+})
